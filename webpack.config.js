b3b8bb500e4cdc064712097d148ff20c3459851c
kv-work <komaroff4@yandex.ru>
2020-01-24 11:59:47 +0500

    add pug-loader, html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,29 @@
-
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-	mode: "development"
+	mode: "development",
+	
+	module: {
+		rules: [
+
+			//Loading .pug
+			{
+				test: /\.pug$/,
+				loader: "pug-loader",
+				options: {
+					pretty: true
+				}
+			}
+
+		]
+	},
+
+	plugins: [
+			new HtmlWebpackPlugin(
+				{
+					filename: "index.html",
+					template: "./src/index.pug"}
+			)
+	]
+	
 }
