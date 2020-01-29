0ebff73312f5c09a82293cdb9dd594343e21e0c8
VyacheslavK <komaroff4@gmail.com>
2020-01-29 18:46:27 +0500

    dropdown pug created

--- src/ui-kits/form-elements/__dropdown/dropdown.js
+++ src/ui-kits/form-elements/__dropdown/dropdown.js
@@ -3,33 +3,34 @@ import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
 $(document).ready( () => {
 
-	// const options = {
-	// 	// max total items
-	// 	maxItems: Infinity,
-	// 	// min total items
-	// 	minItems: 0,
-	// 	// text to show on the dropdown
-	// 	selectionText: 'item',
-	// 	// text to show for multiple items
-	// 	textPlural: 'items',
-	// 	// buttons to increment/decrement
-	// 	controls: {
-	// 		position: 'right',
-	// 		displayCls: 'iqdropdown-item-display',
-	// 		controlsCls: 'iqdropdown-item-controls',
-	// 		counterCls: 'counter'
-	// 	},
-	// 	// fires when an item quantity changes
-	// 	onChange: (id, count, totalItems) => {},
-	// 	// return false to prevent an item decrement
-	// 	beforeDecrement: (id, itemCount) => {},
-	// 	// return false to prevent an item increment
-	// 	beforeIncrement: (id, itemCount) => {}
-	// }
+	const newOptions = {
+		// max total items
+		maxItems: Infinity,
+		// min total items
+		minItems: 0,
+		// text to show on the dropdown
+		selectionText: 'item',
+		// text to show for multiple items
+		textPlural: 'items',
+		// buttons to increment/decrement
+		controls: {
+			position: 'right',
+			displayCls: 'iqdropdown-item-display',
+			controlsCls: 'iqdropdown-item-controls',
+			counterCls: 'counter'
+		},
+		// fires when an item quantity changes
+		onChange: (id, count, totalItems) => {},
+		// return false to prevent an item decrement
+		beforeDecrement: (id, itemCount) => {},
+		// return false to prevent an item increment
+		beforeIncrement: (id, itemCount) => {}
+	}
+
 	const options = {
 		minItems: 1,
     maxItems: 5,
-    selectionText: "passenger",
+    selectionText: "2 спальни, 2 кровати",
     textPlural: "passengers",
     onChange: function(id, count, totalItems) {
       console.log(id, count, totalItems);
