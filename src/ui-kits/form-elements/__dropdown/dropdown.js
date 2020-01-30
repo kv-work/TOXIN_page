9b427f47f43cc97d04b63763c0e7b221efc8348d
kv-work <komaroff4@yandex.ru>
2020-01-30 09:30:29 +0500

    dropdown added

--- src/ui-kits/form-elements/__dropdown/dropdown.js
+++ src/ui-kits/form-elements/__dropdown/dropdown.js
@@ -2,48 +2,19 @@ import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
 $(document).ready( () => {
-
-	// const options = {
-	// 	// max total items
-	// 	maxItems: Infinity,
-	// 	// min total items
-	// 	minItems: 0,
-	// 	// text to show on the dropdown
-	// 	selectionText: 'item',
-	// 	// text to show for multiple items
-	// 	textPlural: 'items',
-	// 	// buttons to increment/decrement
-	// 	controls: {
-	// 		position: 'right',
-	// 		displayCls: 'iqdropdown-item-display',
-	// 		controlsCls: 'iqdropdown-item-controls',
-	// 		counterCls: 'counter'
-	// 	},
-	// 	// fires when an item quantity changes
-	// 	onChange: (id, count, totalItems) => {},
-	// 	// return false to prevent an item decrement
-	// 	beforeDecrement: (id, itemCount) => {},
-	// 	// return false to prevent an item increment
-	// 	beforeIncrement: (id, itemCount) => {}
-	// }
+	
 	const options = {
-		minItems: 1,
-    maxItems: 5,
-    selectionText: "passenger",
-    textPlural: "passengers",
+
     onChange: function(id, count, totalItems) {
-      console.log(id, count, totalItems);
+			console.log(id, count, totalItems);
+			const selectionMod = $.find('p.iqdropdown-selection_modified')
+			console.log(selectionMod)
+			
     },
     beforeDecrement: function(id, itemCount) {
-      if (id === "adult") {
-    	  return itemCount.adult > itemCount.infant;
-			}
 			return true;
 		},
 		beforeIncrement: function(id, itemCount) {
-			if (id === "infant") {
-				return itemCount.adult > itemCount.infant;
-			}
 			return true;
 		}
 	}
