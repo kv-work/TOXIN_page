bda6fff7e2f7aa0586394571c697264a6f895147
kv-work <komaroff4@yandex.ru>
2020-02-06 08:09:00 +0500

    styled filter date dropdown block

--- src/ui-kits/form-elements/__datepicker/datepicker.js
+++ src/ui-kits/form-elements/__datepicker/datepicker.js
@@ -2,14 +2,40 @@ import 'air-datepicker/dist/js/datepicker.min'
 
 $(document).ready( () => {
 
-    const rangeOpt = {
+  $.fn.datepicker.language['my-lang'] = {
+    days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+    daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
+    daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек'],
+    today: 'Сегодня',
+    clear: 'Очистить',
+    dateFormat: 'dd.mm.yyyy',
+    timeFormat: 'hh:ii',
+    firstDay: 1
+  }
+
+  const rangeOpt = {
+      language: 'my-lang',
       range: true,
       toggleSelected: false,
       multipleDatesSeparator: " - ",
+      dateFormat: 'd M',
       clearButton: true
     }
+
+  const startDate = $('.js_datepicker'),
+          endDate   = $('.js_datepicker')[1];
+
+  const datePicker = $('.js_datepicker_ranged').datepicker(rangeOpt)
+
+  const datePickerData = datePicker.data('datepicker')
+
+  $('.js_datepicker').click( (e) => {
     
-    $('.js_datepicker_ranged').datepicker(rangeOpt)
-    $('.js_datepicker').datepicker()
+    e.preventDefault();
+
+    datePickerData.show()
+    })
   }
 )
