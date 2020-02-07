9e9bd3eedf0dcc0e1668e4ad935efffa9f8c07cc
kv-work <komaroff4@yandex.ru>
2020-02-07 14:18:45 +0500

    styled datepicker

--- src/ui-kits/form-elements/__datepicker/datepicker.js
+++ src/ui-kits/form-elements/__datepicker/datepicker.js
@@ -3,64 +3,87 @@ import 'cleave.js'
 
 $(document).ready( () => {
 
-  //Add a mask to enter the date
-  $('.js_datepicker_masked').toArray().forEach(function(field){
-    new Cleave(field, {
-      date: true,
-      datePattern: ['d', 'm', 'Y'],
-      delimiters: ['.','.']
-    });
-  })
+    //Add a mask to enter the date
+    $('.js_datepicker_masked').toArray().forEach(function(field){
+      new Cleave(field, {
+        date: true,
+        datePattern: ['d', 'm', 'Y'],
+        delimiters: ['.','.']
+      });
+    })
 
-  //Set initial value  
-  $('.js_datepicker_masked').toArray().forEach(function(field){
-    const inputValue = $(field).attr('date')
-    $(field).val(inputValue)
-  })
+    //Set initial value  
+    $('.js_datepicker_masked').toArray().forEach(function(field){
+      const inputValue = $(field).attr('date')
+      $(field).val(inputValue)
+    })
 
-  //config datepicker's language
-  $.fn.datepicker.language['my-lang'] = {
-    days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
-    daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
-    daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
-    months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
-    monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек'],
-    today: 'Сегодня',
-    clear: 'Очистить',
-    dateFormat: 'dd.mm.yyyy',
-    timeFormat: 'hh:ii',
-    firstDay: 1
-  }
-
-  //Options
-  const rangeOpt = {
-      language: 'my-lang',
-      range: true,
-      toggleSelected: false,
-      multipleDatesSeparator: " - ",
-      dateFormat: 'd M',
-      clearButton: true,
-      navTitles: {days: 'MM yyyy'},
-      offset: -52, //отступ от начальной позиции
-      onSelect: (formattedDate, date) => console.log(date)
+    //config datepicker's language
+    $.fn.datepicker.language['my-lang'] = {
+      days: ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота'],
+      daysShort: ['Вос','Пон','Вто','Сре','Чет','Пят','Суб'],
+      daysMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+      months: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+      monthsShort: ['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек'],
+      today: 'Сегодня',
+      clear: 'Очистить',
+      dateFormat: 'dd.mm.yyyy',
+      timeFormat: 'hh:ii',
+      firstDay: 1
     }
 
-  //unnecessary test options
-  const altOps = {
-    navTitles: {days: 'MM yyyy'},
-    clearButton: true,
-    inline: true,
-    range: true,
-    toggleSelected: false
-  }
+    //Options
+    const rangeOpt = {
+        language: 'my-lang',
+        range: true,
+        toggleSelected: false,
+        multipleDatesSeparator: " - ",
+        dateFormat: 'd M',
+        clearButton: true,
+        navTitles: {days: 'MM yyyy'},
+        offset: -52, //отступ от начальной позиции
+        onSelect: (_, date) => selectDate(date, startDate, endDate)
+      }
 
-  const startDate = $('.js_datepicker.start_date'),
-          endDate   = $('.js_datepicker.end_date');
+    //unnecessary test options
+    const altOps = {
+      navTitles: {days: 'MM yyyy'},
+      clearButton: true,
+      inline: true,
+      range: true,
+      toggleSelected: false
+    }
 
-  const datePicker = $('.js_datepicker_ranged').datepicker(rangeOpt).data('datepicker')
+    var startDate = $('.js_datepicker.start_date'),
+            endDate = $('.js_datepicker.end_date'),
+            filter = $('.js_datepicker_ranged');
 
-  // datePicker.show()
+    const datePicker = $('.js_datepicker_ranged').datepicker(rangeOpt).data('datepicker')
+    
+    $('.form_datepicker').click( () => datePicker.show() )
 
-  $('.datepicker .datepicker--buttons').append('<span class="datepicker--button" data-action="apply">Применить</span>')
+    $('.datepicker .datepicker--buttons').append('<button type="button" class="datepicker--button-apply">Применить</button>')
+    
+
+    
   }
 )
+
+
+function selectDate(date, startEl, endEl) {
+
+  switch (date.length) {
+    case 1:
+      const startDate = date[0].toLocaleDateString();
+      $(startEl).val(startDate);
+      break;
+    case 2:
+      const endDate = date[1].toLocaleDateString();
+      $(endEl).val(endDate)
+      break;
+    default:
+      $(startEl).val('');
+      $(endEl).val('')
+      break;
+  }
+}
