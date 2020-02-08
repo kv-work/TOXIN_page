f76c4c8d3259f3863f1996e05552e81d84a79dca
VyacheslavK <komaroff4@gmail.com>
2020-02-08 14:26:12 +0500

    refact(dropdown): added a input to use it as a display of selection information

--- src/ui-kits/form-elements/form.js
+++ src/ui-kits/form-elements/form.js
@@ -1,7 +1,7 @@
 //Loading scripts
 import './__input/input'
 import './__datepicker/datepicker'
-// import "./__dropdown/dropdown"     // Ждем обновление (см. описание в dropdown.js)
+import "./__dropdown/dropdown"     // Ждем обновление (см. описание в dropdown.js)
 
 // Строку удалить после настройки скрипта для dropdown'а
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
