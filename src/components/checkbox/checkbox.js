2afbb539703139abeec194e19d457ac0d589d925
kv-work <komaroff4@yandex.ru>
2020-02-14 17:40:21 +0500

    refact: move checkbox-block to components

---
+++ src/components/checkbox/checkbox.js
@@ -0,0 +1,9 @@
+
+
+// $('.form__checkbox_expandable').click( (e) => {
+//   console.log($(e.target));
+//   if (e.target.className != "checkbox__options_wrapper") {
+//     e.stopPropagation;
+//     e.currentTarget.classList.toggle('form__checkbox_expandable_opened');
+//   }  
+// } )
