be348fe7180845a3911c603025eb40fbd619894b
kv-work <komaroff4@yandex.ru>
2020-02-14 18:15:42 +0500

    add(checkbox): add click-event handlener to open/close expandable checkbox list

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,9 +1,28 @@
 
+class ExpandableCheckbox {
+  constructor(checkboxList) {
+    this.$checkboxList = $(checkboxList);
+    this.$checkboxLabel = this.$checkboxList.find('.checkbox__label_wrapper');
+    this.$checkboxOptions = this.$checkboxList.find('.checkbox__options');
+    this.$option = this.$checkboxList.find('.checkbox__option_wrapper');
 
-// $('.form__checkbox_expandable').click( (e) => {
-//   console.log($(e.target));
-//   if (e.target.className != "checkbox__options_wrapper") {
-//     e.stopPropagation;
-//     e.currentTarget.classList.toggle('form__checkbox_expandable_opened');
-//   }  
-// } )
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    const { $checkboxList, $checkboxLabel, $checkboxOptions } = this;
+
+    $checkboxList.click((e) => {
+      $(e.currentTarget).toggleClass('form__checkbox_expandable_opened')
+    })
+
+    $checkboxOptions.click( (e) => {
+      e.stopPropagation()
+    } )
+  }
+
+}
+
+$('.form__checkbox_expandable').each(function() {
+  const checkbox = new ExpandableCheckbox(this);
+})
