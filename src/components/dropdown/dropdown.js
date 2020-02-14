6882fbb345671b43f3ab84abe76983a8d04553da
kv-work <komaroff4@yandex.ru>
2020-02-14 12:54:45 +0500

    add(dropdown): add modificator 'multiple'

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,48 +1,60 @@
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.js';
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
-$(document).ready(() => {
+$('.js_form_dropdown').iqDropdown( {
 
-  const options = {
+  initialText: "Сколько гостей",
+  selectionText: "гость",
+  textPlural: "гостя",
+  moreThenFiveText: "гостей",
+  controls: {
+    controlBtnsCls: 'iqdropdown-menu-control-buttons',
+    clearBtn: true,
+    clearBtnLabel: 'Очистить',
+    applyBtn: true,
+    applyBtnLabel: 'Применить',
+  },
+  onChange: function (id, count, totalItems) {},
+  setCustomMessage(itemCount, totalItems) {
+    if (totalItems == 0) {
+      return this.initialText
+    }
 
-    initialText: "Сколько гостей",
-    selectionText: "гость",
-    textPlural: "гостя",
-    moreThenFiveText: "гостей",
-    controls: {
-      controlBtnsCls: 'iqdropdown-menu-control-buttons',
-      clearBtn: true,
-      clearBtnLabel: 'Очистить',
-      applyBtn: true,
-      applyBtnLabel: 'Применить',
-    },
+    if (totalItems == 1) {
+      return `1 ${this.selectionText}`
+    }
 
+    if (totalItems < 5) {
+      return `${totalItems} ${this.textPlural}`
+    }
 
-    onChange: function (id, count, totalItems) {
-    },
+    if (totalItems >= 5) {
+      return `${totalItems} ${this.moreThenFiveText}`
+    }
+  },
+  onApply: () => {}
+} )
 
-    setSelectionText (itemCount, totalItems) {
+$('.js_form_dropdown_multiple').iqDropdown( {
 
-      if (totalItems == 0) {
-        return this.initialText
-      }
-
-      if (totalItems == 1) {
-        return `1 ${this.selectionText}`
-      }
-
-      if (totalItems < 5) {
-        return `${totalItems} ${this.textPlural}`
-      }
-
-      if (totalItems >= 5) {
-        return `${totalItems} ${this.moreThenFiveText}`
-      }
+  initialText: "Выберите удобства",
+  controls: {
+    controlBtnsCls: 'iqdropdown-menu-control-buttons',
+    clearBtn: false,
+    applyBtn: false,
+  },
+  onChange: function (id, count, totalItems) {},
+  beforeIncrement: (id, itemCount) => itemCount[id] < 4,
+  setCustomMessage(itemCount, totalItems) {
+    if (totalItems == 0) return this.initialText;
+    let selectionText = '';
     
-    },
-    onApply: () => {}
-  }
+    for (let key in itemCount) {
+      if (itemCount[key] > 0) {
+        selectionText += itemCount[key] + " " + key;
+      }
+    }
 
-  $('.js_form_dropdown').iqDropdown(options)
-
-})
+    return selectionText;
+  },
+} )
