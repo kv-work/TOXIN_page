b4ad60cc99fd5d854aaf86a33aa75f0d2c683224
kv-work <komaroff4@yandex.ru>
2020-02-17 11:51:55 +0500

    add(buttons): added event handlers for buttons

--- src/pages/form-elements/form.js
+++ src/pages/form-elements/form.js
@@ -3,6 +3,7 @@ import '../../components/input/input'
 import '../../components/datepicker/datepicker'
 import '../../components/dropdown/dropdown'
 import '../../components/checkbox/checkbox'
+import '../../components/button/button'
 
 import 'item-quantity-dropdown/lib/item-quantity-dropdown.min.css';
 
