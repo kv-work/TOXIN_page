252335a74c8999c8bc6fc76e3211d4f660ebe4aa
kv-work <komaroff4@yandex.ru>
2020-02-18 12:58:48 +0500

    add(range-slider): added script

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -1,11 +1,37 @@
 import 'ion-rangeslider/js/ion.rangeSlider.min'
 import 'ion-rangeslider/css/ion.rangeSlider.min.css'
 
-$(".js_range_slider").ionRangeSlider({
-  type: "double",
-  min: 0,
-  max: 15000,
-  from: 5000,
-  to: 10000,
-  postfix: "₽"
-});
+class Slider {
+  constructor(node) {
+    this.$this = $(node)
+    this.$displayValue = this.$this.find('.range_slider__value')
+    this.$slider = this.$this.find('input')
+    this.$slider.ionRangeSlider()
+    this.sliderData = this.$slider.data('ionRangeSlider')
+
+    this._renderVal(this.$slider, this.$displayValue)
+    this._attachEventHandlers()
+  }
+
+  _attachEventHandlers() {
+    const { $slider, $displayValue, _renderVal} = this;
+
+    $slider.on('change', function() {
+      const $slider = $(this);
+      _renderVal($slider, $displayValue)
+    })
+  }
+
+  _renderVal($slider, $display) {
+    
+    const dataFrom = $slider.data('from') + '₽';
+    const dataTo = $slider.data('to') + '₽';
+    const data = dataFrom + ' - ' + dataTo;
+    $display.html(data)
+  }
+
+}
+
+$('.js_range_slider').each(function() {
+  const slider = new Slider(this)
+})
