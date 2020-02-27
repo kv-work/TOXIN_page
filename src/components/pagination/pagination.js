6f7140a24360f777362884db4edcbe04d6a6a150
VyacheslavK <komaroff4@gmail.com>
2020-02-27 19:15:36 +0500

    fix(pagination): fixed error that occurs when there is no pagination element on the page. Edited styles displaying the cursor on the active page button and the prev/next buttons

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -2,17 +2,39 @@ import 'paginationjs/dist/pagination.min'
 import "paginationjs/dist/pagination.css";
 import './pagination.scss';
 
-$('.js-pagination').pagination({
-  dataSource: function(done){
-    const numberOfPages = $('.js-pagination')[0].dataset.pages;
-    let result = [];
-    for (var i = 1; i <= numberOfPages; i++) {
-        result.push(i);
-    }
-    done(result);
-  },
-  pageSize: 1,
-  pageRange: 1,
-  autoHidePrevious: true,
-  autoHideNext: true,
-})
+class Pagination {
+  constructor(node, options) {
+    this.$node = $(node);
+    this.$pagination = this.$node.find('.js-pagination')
+    this.options = options
+
+    this._init()
+  }
+
+  _init() {
+    const { $pagination, options } = this;
+
+    $pagination.pagination(options)
+  }
+}
+
+const pagination = $('.pagination_block').each( function() {
+  const options = {
+    dataSource: function(done){
+      const numberOfPages = $('.js-pagination')[0].dataset.pages;
+      let result = [];
+      for (var i = 1; i <= numberOfPages; i++) {
+          result.push(i);
+      }
+      done(result);
+    },
+    pageSize: 1,
+    pageRange: 1,
+    autoHidePrevious: true,
+    autoHideNext: true
+  }
+
+  const pagination = new Pagination(this, options)
+} )
+
+
