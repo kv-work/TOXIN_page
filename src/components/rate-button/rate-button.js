350a238d624c366c0c379ac87c3e8c2b4445787e
kv-work <komaroff4@yandex.ru>
2020-03-06 14:15:40 +0500

    work(rate-button): working with rate-button block

--- src/components/rate-button/rate-button.js
+++ src/components/rate-button/rate-button.js
@@ -1,1 +1,31 @@
 import './rate-button.scss'
+
+class RateButton {
+  constructor(node) {
+    this.$node = $(node);
+    this.data = this.$node.data();
+
+    this.$buttons = this.$node.find('input');
+
+    // console.log(this.$buttons)
+    this._init()
+  }
+
+  _init() {
+    const initRate = this.data.rate;
+    // this.setRate(initRate)
+  }
+
+  setRate(rate) {
+    const {$buttons} = this;
+
+    console.log($buttons.eq(rate))
+    console.log(rate)
+
+    $buttons.eq(rate--).change()
+  }
+}
+
+$('.js_rate_button').each(function() {
+  const rateButton = new RateButton(this);
+})
