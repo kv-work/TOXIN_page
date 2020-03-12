007efb02171f306339ed0e8a2f5ff06b542e0fde
kv-work <komaroff4@yandex.ru>
2020-03-12 12:43:25 +0500

    edit (room-rate-card): created class RoomRateCard

--- src/components/room-rate-card/room-rate-card.js
+++ src/components/room-rate-card/room-rate-card.js
@@ -1,1 +1,11 @@
 import './room-rate-card.scss'
+
+class RoomRateCard {
+  constructor(node) {
+    this.$node = $(node);
+  }
+}
+
+$('.js_room_rate_card').each(function() {
+  new RoomRateCard(this)
+})
