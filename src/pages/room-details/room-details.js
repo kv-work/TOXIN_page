557617746631f774f88cc33e54f809e20a4f1a88
VyacheslavK <komaroff4@gmail.com>
2020-03-29 21:13:43 +0500

    created room-details page

---
+++ src/pages/room-details/room-details.js
@@ -0,0 +1,8 @@
+import '../../main.scss'
+import './room-details.scss'
+
+function requireAll(requireContext) {
+  return requireContext.keys().map(requireContext);
+}
+
+requireAll(require.context('../../components', true, /\.(js)$/));
