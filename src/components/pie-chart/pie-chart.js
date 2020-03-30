d24808a903ebea491ce0c0b5ff0dc212060edc09
VyacheslavK <komaroff4@gmail.com>
2020-03-30 17:41:02 +0500

    created pie-chart on svg

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,66 +1,26 @@
 import '../../main.scss';
 import './pie-chart.scss';
+import data from './data.json';
 
-import {GoogleCharts} from 'google-charts';
+class PieChart {
+  constructor(node) {
+    this.$node = $(node);
+    this.$container = this.$node.find('.pie_chart__content');
+    this.data = data.impressions;
 
-GoogleCharts.load(drawCharts);
+    this._init()
+  }
 
-function drawCharts() {
+  _init() {
+    this._addDonutChart()
+  }
 
-  const pieData = GoogleCharts.api.visualization.arrayToDataTable([
-      ['room', 'imprassions'],
-      ['Великолепно', 130],
-      ['Хорошо', 65],
-      ['Удовлетворительно', 65],
-      ['Разочарован', 0]
-    ]);
+  _addDonutChart() {
 
-  const pieOptions = {
-    pieHole: 0.91,
-    // pieStartAngle: 180,
-    reverseCategories: true,
-    sliceVisibilityThreshold: 0,
-    pieSliceTextStyle: {
-      color: 'black'
-    },
-    slices: {
-      3: {color: '#FFE39C'},
-      2: {color: '#6FCF97'},
-      1: {color: '#BC9CFF'},
-      0: {color: '#919191'},  
-    },
-    legend: {
-      position: 'right',
-      alignment: 'end',
-      textStyle: { 
-        color: 'rgba(31, 32, 65, 0.75)',
-        fontSize: 14,
-        fontName: 'Montserrat'
-       }
-    },
-    title: 'Впечатления от номера',
-    titleTextStyle: {
-      color: '#1F2041',
-      fontSize: 19,
-      fontName: 'Quicksand, "Open Sans", sans-serif'
-    },
-    chartArea: {left: 11, top: 24, width: '100%', height: '120px'},
-    pieSliceText: 'none',
-    tooltip: {
-      text: 'value',
-      showColorCode: true,
-      textStyle: {
-        color: 'rgba(31, 32, 65, 0.75)',
-        fontSize: 12,
-        fontName: 'Montserrat',
-
-      }
-    }
-  };
-
-  $('.js_pie_chart').each(function() {
-    const pieChart = new GoogleCharts.api.visualization.PieChart(this);
-    pieChart.draw(pieData, pieOptions);
-  })
+  }
 
 }
+
+$('.js_pie_chart').each(function() {
+  new PieChart(this)
+})
