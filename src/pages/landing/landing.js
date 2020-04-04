93ead3a6297b0bd1265751105d551a4cb6e2e4d9
VyacheslavK <komaroff4@gmail.com>
2020-04-04 21:29:04 +0500

    add (landing-page): added cyclic background change

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -1,5 +1,6 @@
-import '../../main.scss'
-import './landing.scss'
+import '../../main.scss';
+import './landing.scss';
+import data from './data.json';
 
 //import favicon
 import '../../favicons/favicon'
@@ -9,3 +10,40 @@ function requireAll(requireContext) {
 }
 
 requireAll(require.context('../../components', true, /\.(js)$/));
+
+class Landing {
+  constructor(node) {
+    this.$landing = $(node);
+    this.bgArr = data.landingBG;
+    this.currentBG = 0;
+    this.images = [];
+
+    this._init();
+  }
+
+  _init() {
+    this._preloadImages()
+    this._setBGImage(this.currentBG);
+
+    const timerId = setInterval(() => {
+      const img = (this.currentBG + 1) % this.bgArr.length;
+      this._setBGImage(img);
+    }, 10000)
+  }
+
+  _preloadImages() {
+    this.bgArr.forEach(imgUrl => {
+      const image = require(`${imgUrl}`).default;
+      this.images.push(image);
+    });
+  }
+
+  _setBGImage(img) {
+    this.$landing.css({'background-image': `url(${this.images[img]})`});
+    this.currentBG = img;
+  }
+}
+
+$('.js_landing').each(function() {
+  new Landing(this)
+})
