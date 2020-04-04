1b655ba370ca2639334c0f4a1899321f808096fe
VyacheslavK <komaroff4@gmail.com>
2020-04-04 13:30:12 +0500

    add (search-room): added filter-toggler to hide filters

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -9,3 +9,30 @@ function requireAll(requireContext) {
 }
 
 requireAll(require.context('../../components', true, /\.(js)$/));
+
+class SearchRoomContent {
+  constructor(node) {
+    this.$content = $(node);
+    this.$toggler = this.$content.find('.search_room__filter_toggler');
+    this.$filters = this.$content.find('.search_room__filters_block');
+
+    this._init();
+  }
+
+  _init() {
+    this._attachEventHandlers();
+  }
+
+  _attachEventHandlers() {
+    const {$toggler, $filters} = this;
+
+    $toggler.click( () => {
+      $toggler.toggleClass('opened')
+      $filters.toggleClass('opened');
+    } )
+  }
+}
+
+$('.search_room__content').each( function() {
+  new SearchRoomContent(this);
+} )
