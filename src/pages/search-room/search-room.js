6cbef6358fa309456c2ed98fc6e2089dd934e1e1
VyacheslavK <komaroff4@gmail.com>
2020-04-07 14:15:09 +0500

    add (search-room-page): added GET-request generation

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -15,6 +15,11 @@ class SearchRoomContent {
     this.$content = $(node);
     this.$toggler = this.$content.find('.search_room__filter_toggler');
     this.$filters = this.$content.find('.search_room__filters_block');
+    this.$rooms = this.$content.find('.search_room__rooms_block');
+    this.$roomCards = this.$rooms.find('.room_card');
+
+    this.$datepicker = this.$filters.find('.js_form_datepicker');
+    this.$dropdown = this.$filters.find('.js_form_dropdown');
 
     this._init();
   }
@@ -22,6 +27,7 @@ class SearchRoomContent {
   _init() {
     this._attachEventHandlers();
     this._getData();
+    this._setDropdownData();
   }
 
   _getData() {
@@ -37,13 +43,86 @@ class SearchRoomContent {
       }, {});
   }
 
+  _setDropdownData() {
+    const { $dropdown, data } = this;
+    const total = parseInt(data['item-1'])+parseInt(data['item-2'])+parseInt(data['item-3']);
+
+    $dropdown.data({
+      "items": {
+        "item-1": data['item-1'],
+        "item-2": data['item-2'],
+        "item-3": data['item-3']
+      },
+      total: total
+    })
+
+    const $dropdownOptions = $dropdown.find('.iqdropdown-menu-option')
+
+    $dropdownOptions.each( function(idx) {
+      const elemData = $(this).data()
+      $(this).data('defaultcount', data[elemData.id])
+    } )
+  }
+
+  _submitForm(event) {
+    event.preventDefault();
+  }
+
+  _selectRoom(event) {
+    const targetClasses = event.target.classList;
+    
+    if ( !( targetClasses.contains('room_card__control_prev') ||
+            targetClasses.contains('room_card__control_next') ||
+            targetClasses.contains('room_card__indicators') ||
+            targetClasses.contains('room_card__control_icon') ||
+            targetClasses.contains('indicator') ) ) {
+      console.log($(event.currentTarget).data())
+
+      const {$filters} = this;
+      const action = '/room-details';
+      const selectedRoom = $(event.currentTarget).data();
+
+      const formData = this._getFormData();
+
+      formData.append('room', selectedRoom.number);
+
+      let requestString = '?';
+      formData.forEach((value, key) => {
+        requestString += `${key}=${value}&`
+      });
+
+      const encodeString = encodeURI(requestString.slice(0, -1))
+
+      document.location.href = action + encodeString
+    }
+  }
+
+  _getFormData() {
+    const {$filters, $dropdown} = this;
+
+    const formData = new FormData($filters[0]);
+    const dropdownData = $dropdown.data().items;
+
+    for (let key in dropdownData) {
+      formData.set(key, dropdownData[key])
+    }
+
+    return formData;
+  }
+
   _attachEventHandlers() {
-    const {$toggler, $filters} = this;
+    const {$toggler, $filters, $roomCards} = this;
 
     $toggler.click( () => {
       $toggler.toggleClass('opened');
       $filters.toggleClass('opened');
     } )
+
+    $filters.submit( (e) => {
+      this._submitForm(e);
+    } )
+
+    $roomCards.click( (e) => this._selectRoom(e) );
   }
 }
 
