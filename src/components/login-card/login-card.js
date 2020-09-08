509124a7ca814a3e04785fadf859d933131ee4c9
kv-work <komaroff4@yandex.ru>
2020-09-08 14:16:50 +0500

    fix (login-card): fixed eslint errors

--- src/components/login-card/login-card.js
+++ src/components/login-card/login-card.js
@@ -1,10 +1,11 @@
 import './login-card.scss';
+import $ from 'jquery';
 
 class LoginCard {
   constructor(node) {
     this.$form = $(node);
 
-    this._init()
+    this._init();
   }
 
   _init() {
@@ -14,33 +15,32 @@ class LoginCard {
   _attachEventHandlers() {
     const { $form } = this;
 
-    $form.submit( e => this._submitForm(e) )
+    $form.on('submit', this._submitForm.bind(this));
   }
 
   async _submitForm(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    const {$form} = this;
+    const { $form } = this;
 
-    const formData = new FormData($form[0])
+    const formData = new FormData($form[0]);
     const url = $form[0].action;
 
     const response = await fetch(url, {
       method: 'POST',
       body: formData,
-    })
+    });
 
     if (!response.ok) {
-      // throw new Error(`Не удалость отправить данные по адресу ${url}. Статус: ${response.status}`)
-      console.log(`Не удалость отправить данные по адресу ${url}. Статус: ${response.status}`)
+      throw new Error(`Не удалость отправить данные по адресу ${url}. Статус: ${response.status}`);
     } else {
-      // Получаем ответ
-      return await response.json()
+      // const json = await response.json();
     }
-
   }
 }
 
-$('.login_card').each(function() {
-  new LoginCard(this)
-})
+$('.js-login_card').each((_, node) => {
+  const loginCard = new LoginCard(node);
+
+  return loginCard;
+});
