3983d35b783c5d2f2336959b52273efd6f97a418
kv-work <komaroff4@yandex.ru>
2020-09-08 14:08:10 +0500

    fix (like-button): fixed eslint errors

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,32 +1,38 @@
-import './like-button.scss'
+import $ from 'jquery';
+import './like-button.scss';
 
 class LikeButton {
   constructor(node) {
     this.$this = $(node);
-    this.value = this.$this.find('p').text();
     this.$input = this.$this.find('.like_button__input');
 
-    this._attachEventHandlers()
+    this._attachEventHandlers();
   }
 
   _attachEventHandlers() {
-    const { $this, $input, _increase, _decrease } = this;
-    let { value } = this;
+    const { $this } = this;
 
-    $this.change(function() {
-      if (this.control.checked) {
-        this.classList.add('like_button__label_checked')
-        ++value
-      } else {
-        this.classList.remove('like_button__label_checked')
-        --value
-      }
+    $this.on('change', LikeButton.changeHandler);
+  }
 
-      $(this).find('p').text(value)
-    })
+  static changeHandler(event) {
+    const { currentTarget } = event;
+    let value = $(currentTarget).find('p').text();
+
+    if (currentTarget.control.checked) {
+      currentTarget.classList.add('like_button__label_checked');
+      value = +value + 1;
+    } else {
+      currentTarget.classList.remove('like_button__label_checked');
+      value = +value - 1;
+    }
+
+    $(currentTarget).find('p').text(value);
   }
 }
 
-$('.like_button').each(function() {
-  const likeButton = new LikeButton(this)
-})
+$('.js-like_button').each((_, node) => {
+  const likeButton = new LikeButton(node);
+
+  return likeButton;
+});
