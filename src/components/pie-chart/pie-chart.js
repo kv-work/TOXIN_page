3df38ec2b82eac75046a87dc576a85d4373be4eb
kv-work <komaroff4@yandex.ru>
2020-09-09 16:46:04 +0500

    fix (pie-chart): fixed eslint errors

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import '../../main.scss';
 import './pie-chart.scss';
 import data from './data.json';
@@ -6,53 +7,47 @@ class PieChart {
   constructor(node) {
     this.$node = $(node);
     this.$container = this.$node.find('.pie_chart__content');
-    this.$legend = this.$node.find('.pie_chart__legend')
-    this.data = data.impressions;
+    this.$legend = this.$node.find('.pie_chart__legend');
+    this.impressions = data.impressions;
 
-    this._init()
-  }
-
-  _init() {
-    this._addDonutChart()
+    this._addDonutChart();
   }
 
   _addDonutChart() {
-    const { data } = this;
+    const { impressions } = this;
 
     const radius = 58;
-
-    const donutChart = $(document.createElementNS("http://www.w3.org/2000/svg", "svg")).attr({
+    const donutChart = $(document.createElementNS('http://www.w3.org/2000/svg', 'svg')).attr({
       class: 'chart',
       width: '100%',
       height: '100%',
-      viewBox: '0 0 120 120'
+      viewBox: '0 0 120 120',
     });
-    const donutHole = $(document.createElementNS("http://www.w3.org/2000/svg", "circle")).attr({
+    const donutHole = $(document.createElementNS('http://www.w3.org/2000/svg', 'circle')).attr({
       class: 'chart__hole',
       cx: 60,
       cy: 60,
       r: radius,
-      fill: '#fff'
+      fill: '#fff',
     });
 
-    const chartLegend = $('<ul>', {class: 'pie_chart__legend_list'});
+    const chartLegend = $('<ul>', { class: 'pie_chart__legend_list' });
 
     donutChart.append(donutHole);
 
     let sumOfImpressions = 0;
-
-    for (const key in data) {
-      sumOfImpressions += data[key]
-    };
-
-    const sumOfImpressionsDisplay = this._createTextDisplay(sumOfImpressions);
-
-
     let currentCount = 0;
+    const keys = Object.keys(impressions);
 
-    for (const key in data) {
+    keys.forEach((key) => {
+      sumOfImpressions += impressions[key];
+    });
+
+    const sumOfImpressionsDisplay = PieChart._createTextDisplay(sumOfImpressions);
+
+    keys.forEach((key) => {
       let color = 'primary_grad';
-      let label = 'Великолепно'
+      let label = 'Великолепно';
 
       switch (key) {
         case 'Great':
@@ -73,16 +68,16 @@ class PieChart {
           break;
         default:
           break;
-      };
+      }
 
-      currentCount += data[key];
+      currentCount += impressions[key];
 
       const circumference = 2 * radius * Math.PI;
       const segmentPercent = currentCount / sumOfImpressions;
-      const segmentVal = data[key] / sumOfImpressions * circumference;
+      const segmentVal = (impressions[key] / sumOfImpressions) * circumference;
 
-      if (data[key] != 0) {
-        const donutSegment = $(document.createElementNS("http://www.w3.org/2000/svg", "circle")).attr({
+      if (impressions[key] !== 0) {
+        const donutSegment = $(document.createElementNS('http://www.w3.org/2000/svg', 'circle')).attr({
           class: 'chart__segment',
           cx: 60,
           cy: 60,
@@ -90,50 +85,49 @@ class PieChart {
           fill: 'transparent',
           stroke: `url(#${color})`,
           'stroke-width': 4,
-          'stroke-dasharray': `${segmentVal - 2 } ${circumference - segmentVal + 2}`,
-          'stroke-dashoffset': `${(segmentPercent + 0.25) * circumference - 1}`
+          'stroke-dasharray': `${segmentVal - 2} ${circumference - segmentVal + 2}`,
+          'stroke-dashoffset': `${(segmentPercent + 0.25) * circumference - 1}`,
         });
 
         donutChart.append(donutSegment);
-      }      
+      }
 
-      const chartLegendItem = $('<li>', {class: `pie_chart__legend_list_item_color_${color}`}).html(label);
+      const chartLegendItem = $('<li>', { class: `pie_chart__legend_list_item_color_${color}` }).html(label);
 
       chartLegend.append(chartLegendItem);
-    }
+    });
 
-    const primeryGradient = this._createGradient('primary_grad', '#BC9CFF', '#8BA4F9');
-    const secondaryGradient = this._createGradient('secondary_grad', '#6FCF97', '#66D2EA');
-    const yellowGradient = this._createGradient('yellow_grad', '#FFE39C', '#FFBA9C');
-    const blackGradient = this._createGradient('black_grad', '#919191', '#3D4975');
+    const primeryGradient = PieChart._createGradient('primary_grad', '#BC9CFF', '#8BA4F9');
+    const secondaryGradient = PieChart._createGradient('secondary_grad', '#6FCF97', '#66D2EA');
+    const yellowGradient = PieChart._createGradient('yellow_grad', '#FFE39C', '#FFBA9C');
+    const blackGradient = PieChart._createGradient('black_grad', '#919191', '#3D4975');
 
     donutChart
       .append(sumOfImpressionsDisplay)
       .append(primeryGradient)
       .append(secondaryGradient)
       .append(yellowGradient)
-      .append(blackGradient)
+      .append(blackGradient);
 
-    this.$container.append(donutChart)
-    this.$legend.append(chartLegend)
+    this.$container.append(donutChart);
+    this.$legend.append(chartLegend);
   }
 
-  _createTextDisplay(num) {
-    const textDisplay = 
-      $(document.createElementNS("http://www.w3.org/2000/svg", 'g')).attr({ class: 'chart__sum_of_impress' });
-    
-    const numLabel = $(document.createElementNS("http://www.w3.org/2000/svg", 'text')).attr({
+  static _createTextDisplay(num) {
+    const textDisplay = $(document.createElementNS('http://www.w3.org/2000/svg', 'g')).attr({ class: 'chart__sum_of_impress' });
+
+    const numLabel = $(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr({
       class: 'chart__num_label',
       'text-anchor': 'middle',
       x: '50%',
-      y: '49%'
+      y: '49%',
     }).html(num);
 
-    const textLabel = $(document.createElementNS("http://www.w3.org/2000/svg", 'text')).attr({
+    const textLabel = $(document.createElementNS('http://www.w3.org/2000/svg', 'text')).attr({
       class: 'chart__text_label',
       'text-anchor': 'middle',
       x: '50%',
-      y: '64%'
+      y: '64%',
     }).html('голосов');
 
     textDisplay
@@ -143,28 +137,29 @@ class PieChart {
     return textDisplay;
   }
 
-  _createGradient(id, color1, color2) {
-    const linearGradiend = $(document.createElementNS("http://www.w3.org/2000/svg", "linearGradient")).attr({
-      id: id,
+  static _createGradient(id, color1, color2) {
+    const linearGradiend = $(document.createElementNS('http://www.w3.org/2000/svg', 'linearGradient')).attr({
+      id,
       x1: 60,
       y1: 0,
       x2: 60,
       y2: 120,
-      gradientUnits: 'userSpaceOnUse'
-    })
+      gradientUnits: 'userSpaceOnUse',
+    });
 
-    const stop1 = $(document.createElementNS("http://www.w3.org/2000/svg", "stop")).attr({'stop-color': color1})
-    const stop2 = $(document.createElementNS("http://www.w3.org/2000/svg", "stop")).attr({'offset': 1, 'stop-color': color2})
+    const stop1 = $(document.createElementNS('http://www.w3.org/2000/svg', 'stop')).attr({ 'stop-color': color1 });
+    const stop2 = $(document.createElementNS('http://www.w3.org/2000/svg', 'stop')).attr({ offset: 1, 'stop-color': color2 });
 
     linearGradiend
       .append(stop1)
-      .append(stop2)
+      .append(stop2);
 
-    return linearGradiend
+    return linearGradiend;
   }
-
 }
 
-$('.js_pie_chart').each(function() {
-  new PieChart(this)
-})
+$('.js-pie_chart').each(function addPieChart() {
+  const pieChart = new PieChart(this);
+
+  return pieChart;
+});
