1068d7d47bf6538abb5975910c7663976cf929c4
kv-work <komaroff4@yandex.ru>
2020-09-13 11:06:17 +0500

    fix (room-images): fixed eslint errors

--- src/components/room-images/room-images.js
+++ src/components/room-images/room-images.js
@@ -1,13 +1,13 @@
 import '../../main.scss';
 import './room-images.scss';
+import $ from 'jquery';
+import './img/image-loader';
 import data from './data.json';
 
 class RoomImagesBlock {
   constructor(node) {
     this.$node = $(node);
-    this.room = this.$node.data().room;
-
-    this.images = data.images[this.room];
+    this.room = this.$node.data('room');
 
     this._init();
   }
@@ -19,20 +19,18 @@ class RoomImagesBlock {
   }
 
   _addImages() {
-    const {images, $node} = this;
+    const images = data.images[this.room];
+    const { $node } = this;
 
-    images.forEach( (element, idx) => {
-
-      const $image = $('<div>', { class: 'room_images__image'});
-      const imgUrl = require(`${element}`).default;
-      
+    images.forEach((element, idx) => {
+      const $image = $('<div>', { class: 'room_images__image' });
       $image.css({
-        'background-image': `url(${imgUrl})`,
+        'background-image': `url(${element})`,
       });
 
       if (idx === 0) {
         $image.addClass('active');
-        $node.data({'active-slide': 0});
+        $node.data({ 'active-slide': 0 });
       }
 
       $node.append($image);
@@ -40,37 +38,38 @@ class RoomImagesBlock {
   }
 
   _createCarouselcontrols() {
-    const {images, $node} = this;
+    const images = data.images[this.room];
+    const { $node } = this;
 
-    this.$indicators = $('<ol>', {class: 'room_images__indicators_block'});
-    this.$controlPrev = $('<div>', {class: 'room_images__control_prev'});
-    this.$controlNext = $('<div>', {class: 'room_images__control_next'});
+    this.$indicators = $('<ol>', { class: 'room_images__indicators_block' });
+    this.$controlPrev = $('<div>', { class: 'room_images__control_prev' });
+    this.$controlNext = $('<div>', { class: 'room_images__control_next' });
 
     const $controlIconPrev = $('<i>', {
       class: 'material-icons room_images__control_icon',
-      text: 'expand_more'
-    })
+      text: 'expand_more',
+    });
     const $controlIconNext = $('<i>', {
       class: 'material-icons room_images__control_icon',
-      text: 'expand_more'
-    })
+      text: 'expand_more',
+    });
 
-    this.$controlPrev.append($controlIconPrev)
-    this.$controlNext.append($controlIconNext)
+    this.$controlPrev.append($controlIconPrev);
+    this.$controlNext.append($controlIconNext);
 
-    images.forEach( (_, idx) => {
+    images.forEach((_, idx) => {
       const $indicator = $('<li>', {
         class: 'room_images__indicator',
-        'data-slide-to': idx
-      })
+        'data-slide-to': idx,
+      });
 
       if (idx === 0) {
         $indicator.addClass('active');
-        this.$indicators.data({'active-slide': 0});
+        this.$indicators.data({ 'active-slide': 0 });
       }
 
       this.$indicators.append($indicator);
-    })
+    });
 
     if (images.length > 1) {
       $node
@@ -82,56 +81,66 @@ class RoomImagesBlock {
   }
 
   _changeSlide(slide) {
-    const {$indicators, $node, images} = this;
+    const images = data.images[this.room];
+    const { $indicators, $node } = this;
     const numOfSlides = images.length;
     const currentSlide = $node.data().activeSlide;
 
-    if (slide != currentSlide && slide <= numOfSlides) {
-      $node.find('.room_images__image').eq(currentSlide).removeClass('active')
-      $node.find('.room_images__image').eq(slide).addClass('active')
-  
-      $indicators.find('li').eq(currentSlide).removeClass('active')
-      $indicators.find('li').eq(slide).addClass('active')
-          
-      $node.data({'active-slide': slide})
-      $indicators.data({'active-slide': slide})
+    if (slide !== currentSlide && slide <= numOfSlides) {
+      $node.find('.room_images__image').eq(currentSlide).removeClass('active');
+      $node.find('.room_images__image').eq(slide).addClass('active');
+
+      $indicators.find('li').eq(currentSlide).removeClass('active');
+      $indicators.find('li').eq(slide).addClass('active');
+
+      $node.data({ 'active-slide': slide });
+      $indicators.data({ 'active-slide': slide });
     }
   }
 
   _attachEventHandlers() {
-    const {$indicators, $controlNext, $controlPrev, $node, images} = this;
+    const {
+      $indicators,
+      $controlNext,
+      $controlPrev,
+    } = this;
+
+    $indicators.on('click', this._clickOnIndicatorsHandler.bind(this));
+
+    $controlPrev.on('click', this._clickOnCtrlBtnsHandler.bind(this));
+
+    $controlNext.on('click', this._clickOnCtrlBtnsHandler.bind(this));
+  }
+
+  _clickOnIndicatorsHandler(event) {
+    const $target = $(event.target);
+    const { slideTo } = $target.data();
+
+    if ($target.hasClass('room_images__indicator')) {
+      this._changeSlide(slideTo);
+    }
+  }
+
+  _clickOnCtrlBtnsHandler(event) {
+    const images = data.images[this.room];
     const numOfSlides = images.length;
+    const currentSlide = this.$node.data().activeSlide;
+    const controlBtn = event.target;
+    if (controlBtn.classList.contains('room_images__control_icon')) {
+      const slide = (currentSlide - 1 + numOfSlides) % numOfSlides;
 
-    $indicators.click( (e) => {
-      const $target = $(e.target)
+      this._changeSlide(slide);
+    }
+    if (controlBtn.classList.contains('room_images__control_icon')) {
+      const slide = (currentSlide + 1) % numOfSlides;
 
-      if ( $target.hasClass('room_images__indicator') ) {
-        const slideTo = $(e.target).data().slideTo;
-
-        this._changeSlide(slideTo)
-      }
-    })
-
-    $controlPrev.click( (e) => {
-      if ( e.target.classList.contains('room_images__control_icon') ) {
-        const currentSlide = $node.data().activeSlide;
-        const slide = (currentSlide - 1 + numOfSlides) % numOfSlides;
-
-        this._changeSlide(slide);
-      }
-    })
-
-    $controlNext.click( (e) => {
-      if ( e.target.classList.contains('room_images__control_icon') ) {
-        const currentSlide = $node.data().activeSlide;
-        const slide = (currentSlide + 1) % numOfSlides;
-
-        this._changeSlide(slide);
-      }
-    })
+      this._changeSlide(slide);
+    }
   }
 }
 
-$('.js_room_images').each(function() {
-  new RoomImagesBlock(this)
-})
+$('.js-room_images').each(function addRoomImages() {
+  const roomImages = new RoomImagesBlock(this);
+
+  return roomImages;
+});
