e5f0ed23bf2f2c7e44f2e96938a87a1088ebe894
kv-work <komaroff4@yandex.ru>
2020-11-09 15:36:59 +0500

    #15 Changed dropdown menu nav-bar

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,1 +1,45 @@
+import $ from 'jquery';
+
 import './nav-bar.scss';
+
+class NestedItem {
+  constructor(node) {
+    this.$nestedItem = $(node);
+
+    this._attachEventHandler();
+  }
+
+  _attachEventHandler() {
+    const { $nestedItem } = this;
+
+    $nestedItem.on('click', NestedItem.clickHandler.bind(this));
+  }
+
+  static clickHandler(e) {
+    const { target } = e;
+
+    $(target).find('ul').toggle();
+
+    setTimeout(() => {
+      $(document).on('click', NestedItem.makeNewClickHandler(target));
+    }, 0);
+  }
+
+  static makeNewClickHandler(node) {
+    const newClickHandler = (e) => {
+      if (e.target !== node) {
+        $(node).find('ul').hide();
+      }
+
+      $(document).off('click', newClickHandler);
+    };
+
+    return newClickHandler;
+  }
+}
+
+$('.js-item_nested').each(function createNestedItem() {
+  const nestedItem = new NestedItem(this);
+
+  return nestedItem;
+});
