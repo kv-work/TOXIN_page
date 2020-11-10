b2288f9602c8b7cea60b49d2bc9a22c05dd2c229
kv-work <komaroff4@yandex.ru>
2020-11-10 16:26:18 +0500

    changed click event handler for options in ExpandableCheckboxList

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -11,14 +11,16 @@ class ExpandableCheckboxList {
   _attachEventHandlers() {
     const { $list, $options } = this;
 
-    $options.click((e) => {
-      e.stopPropagation();
-    });
+    $options.on('click', ExpandableCheckboxList._optionClickHandler);
     $list.on('click', this._toggleClass.bind(this));
   }
 
   _toggleClass() {
     this.$list.toggleClass('form__checkbox_expandable_opened');
+  }
+
+  static _optionClickHandler(e) {
+    e.stopPropagation();
   }
 }
 
