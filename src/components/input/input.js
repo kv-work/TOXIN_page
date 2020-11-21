afb64965514cae59c342482cdb5891697f94de6b
VyacheslavK <komaroff4@yandex.ru>
2020-11-21 11:48:06 +0500

    refact (input): refactored component

--- src/components/input/input.js
+++ src/components/input/input.js
@@ -1,14 +1,12 @@
 import $ from 'jquery';
 import Cleave from 'cleave.js';
 
-$(document).ready(() => {
-  $('.js-form_input__input_masked').each((_, field) => {
-    const input = new Cleave(field, {
-      date: true,
-      datePattern: ['d', 'm', 'Y'],
-      delimiters: ['.', '.'],
-    });
-
-    return input;
+$('.js-input__input_masked').each((_, field) => {
+  const input = new Cleave(field, {
+    date: true,
+    datePattern: ['d', 'm', 'Y'],
+    delimiters: ['.', '.'],
   });
+
+  return input;
 });
