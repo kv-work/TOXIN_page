1ec4f9720f8e858089d92b4e28c0bb66d8885c05
kv-work <komaroff4@yandex.ru>
2020-12-28 15:11:31 +0500

    #22 transfered datepicker config from  callback

--- src/components/datepicker-block/datepicker-block.js
+++ src/components/datepicker-block/datepicker-block.js
@@ -1,34 +1,34 @@
 import $ from 'jquery';
 import Datepicker from './Datepicker';
 
+// config datepicker's language
+$.fn.datepicker.language['my-lang'] = {
+  days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+  daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
+  daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+  months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+  monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
+  today: 'Сегодня',
+  clear: 'Очистить',
+  dateFormat: 'dd.mm.yyyy',
+  timeFormat: 'hh:ii',
+  firstDay: 1,
+};
+
+const options = {
+  language: 'my-lang',
+  range: true,
+  toggleSelected: false,
+  multipleDatesSeparator: ' - ',
+  dateFormat: 'd M',
+  clearButton: true,
+  offset: 15,
+  navTitles: {
+    days: 'MM yyyy',
+  },
+};
+
 $('.js-datepicker-block').each(function addDatepicker() {
-  // config datepicker's language
-  $.fn.datepicker.language['my-lang'] = {
-    days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    daysShort: ['Вос', 'Пон', 'Вто', 'Сре', 'Чет', 'Пят', 'Суб'],
-    daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-    months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-    monthsShort: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
-    today: 'Сегодня',
-    clear: 'Очистить',
-    dateFormat: 'dd.mm.yyyy',
-    timeFormat: 'hh:ii',
-    firstDay: 1,
-  };
-
-  const options = {
-    language: 'my-lang',
-    range: true,
-    toggleSelected: false,
-    multipleDatesSeparator: ' - ',
-    dateFormat: 'd M',
-    clearButton: true,
-    offset: 15,
-    navTitles: {
-      days: 'MM yyyy',
-    },
-  };
-
   const datepicker = new Datepicker(this, options);
 
   return datepicker;
